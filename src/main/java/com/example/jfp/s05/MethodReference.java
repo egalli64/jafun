8df39cf9284ac198fd6b669732e7471129526651
Emanuele <egalli64@gmail.com>
2022-02-28 18:06:11 +0100

    method reference

---
+++ src/main/java/com/example/jfp/s05/MethodReference.java
@@ -0,0 +1,35 @@
+package com.example.jfp.s05;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReference {
+    public static void main(String[] args) {
+        String[] names = { "tom", "Bob", "kelly", "Ted" };
+        System.out.println("As created: " + Arrays.toString(names));
+
+        Arrays.sort(names);
+        System.out.println("Natural order: " + Arrays.toString(names));
+
+        // lambda
+        Arrays.sort(names, (l, r) -> l.compareToIgnoreCase(r));
+        System.out.println("Natural order (ignore case) by lambda: " + Arrays.toString(names));
+
+        // back to natural order
+        Arrays.sort(names);
+
+        // method reference
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println("Natural order (ignore case) by method reference: " + Arrays.toString(names));
+
+        List<String> list = List.of(names);
+        
+        // lambda
+        System.out.println("Print each element by lambda");
+        list.forEach(s -> System.out.print(s));
+        
+        // method reference
+        System.out.println("Print each element by method reference");
+        list.forEach(System.out::println);
+    }
+}
