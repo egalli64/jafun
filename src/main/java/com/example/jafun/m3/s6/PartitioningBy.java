bc5faa39ad4c348f0f6a8093d753bdb655ef420b
Emanuele <egalli64@gmail.com>
2024-07-14 20:26:49 +0200

    module split

--- src/main/java/com/example/jafun/m3/s6/PartitioningBy.java
+++ src/main/java/com/example/jafun/m3/s6/PartitioningBy.java
@@ -1,11 +1,11 @@
 /*
  * Introduction to Functional Programming with Java
  * 
- * Module 1 - Stream
+ * Module 3 - collect from Stream
  * 
  * https://github.com/egalli64/jafun
  */
-package com.example.jafun.m1.s17;
+package com.example.jafun.m3.s6;
 
 import java.util.List;
 import java.util.Map;
