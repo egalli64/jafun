9fc2e4728fe797ca037a0364155790d50a59046a
Emanuele <egalli64@gmail.com>
2024-07-22 14:58:26 +0200

    more info

--- src/main/java/com/example/jafun/m1/s01/LambdaExample.java
+++ src/main/java/com/example/jafun/m1/s01/LambdaExample.java
@@ -11,20 +11,25 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * How to pass code to a method by lambda
+ * How to pass code to a stream method by lambda
  */
 public class LambdaExample {
     public static void main(String[] args) {
         Collection<String> data = List.of("first", "second", "third");
         System.out.println("The input list: " + data);
 
-        System.out.println("\nPrint each element by explicit lambda");
+        System.out.println("\nPrint each element in the imperative way");
+        for (String s : data) {
+            System.out.println(s);
+        }
+
+        System.out.println("\nSame by stream, invoking forEach(), passing an explicit lambda to it");
         data.stream().forEach((String s) -> System.out.println(s));
 
-        System.out.println("\nBy compact lambda");
+        System.out.println("\nCompact lambda");
         data.stream().forEach(s -> System.out.println(s));
 
-        System.out.println("\nBy method reference");
+        System.out.println("\nMethod reference");
         data.stream().forEach(System.out::println);
     }
 }
