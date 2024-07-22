6abc71fd323384409eb48c0a9b72cd5db8f0d29b
Emanuele <egalli64@gmail.com>
2024-07-22 16:04:53 +0200

    minor review

--- src/main/java/com/example/jafun/m2/s7/FunctionalPrimitiveExample.java
+++ src/main/java/com/example/jafun/m2/s7/FunctionalPrimitiveExample.java
@@ -1,11 +1,11 @@
 /*
  * Introduction to Functional Programming with Java
  * 
- * Module 2 - FP
+ * Module 2 - FP by lambda expression
  * 
  * https://github.com/egalli64/jafun
  */
-package com.example.jafun.m2.s07;
+package com.example.jafun.m2.s7;
 
 import java.util.function.DoublePredicate;
 import java.util.function.DoubleToIntFunction;
