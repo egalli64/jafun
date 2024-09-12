5c3b782ee3d158c11bacb1650d58206a85bc837a
Emanuele <egalli64@gmail.com>
2024-09-12 21:35:38 +0200

    Method reference example

---
+++ src/main/java/com/example/jafun/m1/s5/MethRef3ActionListenerDeduced.java
@@ -0,0 +1,26 @@
+/*
+ * Introduction to Functional Programming with Java
+ * 
+ * https://github.com/egalli64/jafun
+ */
+package com.example.jafun.m1.s5;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Lambda syntax
+ */
+public class MethRef3ActionListenerDeduced {
+    /**
+     * Defining an ActionListener lambda by method reference
+     * <p>
+     * Invoke the println() method of System.out on the passed argument
+     */
+    public static void main(String[] args) {
+        System.out.println("An ActionListener by method reference\n");
+        ActionListener lambda = System.out::println;
+
+        lambda.actionPerformed(new ActionEvent(new Object(), 42, "Hello"));
+    }
+}
