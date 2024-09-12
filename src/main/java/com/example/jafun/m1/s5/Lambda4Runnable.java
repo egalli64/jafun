65f275fe9e480fb9acdf25f43820af375b8c71ea
Emanuele <egalli64@gmail.com>
2024-09-12 21:40:51 +0200

    review

---
+++ src/main/java/com/example/jafun/m1/s5/Lambda4Runnable.java
@@ -0,0 +1,23 @@
+/*
+ * Introduction to Functional Programming with Java
+ * 
+ * https://github.com/egalli64/jafun
+ */
+package com.example.jafun.m1.s5;
+
+/**
+ * Lambda syntax
+ */
+public class Lambda4Runnable {
+    /**
+     * Defining a Runnable lambda
+     * <p>
+     * Print a message
+     */
+    public static void main(String[] args) {
+        System.out.println("Running a runnable\n");
+        Runnable runner = () -> System.out.println("Running ...");
+
+        runner.run();
+    }
+}
